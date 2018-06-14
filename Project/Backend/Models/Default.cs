0ace5d809094406289291bec45d739fc45adcf06
hodoje <nikolakaraklic@gmail.com>
2018-06-14 23:39:28 +0200

    Changed route template. Added default controller and default model for testing.

---
+++ Project/Backend/Models/Default.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.Models
+{
+    public class Default
+    {
+        public string DefaultName { get; set; }
+        public string DefaultLastName { get; set; }
+    }
+}
