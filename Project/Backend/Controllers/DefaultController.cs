7f512e1fecf02609b9170d31bdb41a2c7cdcc339
hodoje <nikolakaraklic@gmail.com>
2018-06-14 23:52:09 +0200

    Added default action for default controller.

--- Project/Backend/Controllers/DefaultController.cs
+++ Project/Backend/Controllers/DefaultController.cs
@@ -4,10 +4,15 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Backend.Models;
 
 namespace Backend.Controllers
 {
     public class DefaultController : ApiController
     {
+        public Default GetDefault()
+        {
+            return new Default {DefaultName = "Nikola", DefaultLastName = "Karaklic"};
+        }
     }
 }
