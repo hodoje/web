403f613460d38a252428dae7ce29285146e1c91a
hodoje <nikolakaraklic@gmail.com>
2018-06-21 21:33:33 +0200

    Added initial components and services.

--- Project/Frontend/TaxiService/src/app/app.module.ts
+++ Project/Frontend/TaxiService/src/app/app.module.ts
@@ -1,16 +1,31 @@
+import { RegistrationService } from './services/registration.service';
+import { UsersService } from './services/users.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContentComponent } from './components/content/content.component';
+import { RidesService } from './services/rides.service';
+import { LoginService } from './services/login.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    LoginService,
+    RegistrationService,
+    UsersService,
+    RidesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
