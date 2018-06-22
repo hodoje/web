129b6c33d5d528622af429adb90e91698504c9bc
hodoje <nikolakaraklic@gmail.com>
2018-06-22 14:54:51 +0200

    Implemented simple generic service.

--- Project/Frontend/TaxiService/src/app/services/generic.service.ts
+++ Project/Frontend/TaxiService/src/app/services/generic.service.ts
@@ -1,10 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class GenericService {
+export abstract class GenericService {
 
-  constructor() { }
+  constructor(private url: string, private endpoint: string, private httpClient: HttpClient) { }
+
+  public getById(id: number){
+    return this.httpClient.get(`${this.url}/${this.endpoint}/${id}`);
+  }
+
+  public getAll(){
+    return this.httpClient.get(`${this.url}/${this.endpoint}`);
+  }
+
+  public post(item: any){
+    return this.httpClient.post(`${this.url}/${this.endpoint}`, item);
+  }
+
+  public put(id: number, item:any){
+    return this.httpClient.put(`${this.url}/${this.endpoint}/${id}`, item);
+  }
+
+  public delete(id: number){
+    return this.httpClient.delete(`${this.url}/${this.endpoint}/${id}`);
+  }
 }
