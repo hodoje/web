84fa10f116bd7c8bcab1c73b361f04f399bcda76
hodoje <nikolakaraklic@gmail.com>
2018-06-25 16:12:43 +0200

    Simple try to get own data from the backend.

--- Project/Frontend/TaxiService/src/app/components/customer/customer.component.ts
+++ Project/Frontend/TaxiService/src/app/components/customer/customer.component.ts
@@ -1,4 +1,7 @@
+import { UsersService } from './../../services/users.service';
 import { Component, OnInit } from '@angular/core';
+import { ApiMessage } from '../../models/apiMessage.model';
+import { LoginModel } from '../../models/login.model';
 
 @Component({
   selector: 'customer',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerComponent implements OnInit {
 
-  constructor() { }
+  
+
+  constructor(private userService: UsersService) { }
 
   ngOnInit() {
   }
 
+  getUserByUsername(){
+    let apiMessage = new ApiMessage(localStorage.userHash, new LoginModel(null, null, localStorage.role));
+    this.userService.getUserByUsername(apiMessage).subscribe(data => {console.log(data);});
+  }
 }
--- Project/Frontend/TaxiService/src/app/components/driver/driver.component.ts
+++ Project/Frontend/TaxiService/src/app/components/driver/driver.component.ts
@@ -1,4 +1,7 @@
+import { UsersService } from './../../services/users.service';
 import { Component, OnInit } from '@angular/core';
+import { ApiMessage } from '../../models/apiMessage.model';
+import { LoginModel } from '../../models/login.model';
 
 @Component({
   selector: 'driver',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DriverComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UsersService) { }
 
   ngOnInit() {
   }
 
+  getUserByUsername(){
+    let apiMessage = new ApiMessage(localStorage.userHash, new LoginModel(null, null, localStorage.role));
+    this.userService.getUserByUsername(apiMessage).subscribe(data => {console.log(data);});
+  }
 }
