2892184f462af980a566ab4d802967bb5c13ca81
hodoje <nikolakaraklic@gmail.com>
2018-06-28 21:44:44 +0200

    Driver can now get its own personal data.

--- Project/Frontend/TaxiService/src/app/components/driver/driver.component.ts
+++ Project/Frontend/TaxiService/src/app/components/driver/driver.component.ts
@@ -1,7 +1,11 @@
+import { Location } from './../../models/location.model';
 import { UsersService } from './../../services/users.service';
 import { Component, OnInit } from '@angular/core';
 import { ApiMessage } from '../../models/apiMessage.model';
 import { LoginModel } from '../../models/login.model';
+import { User } from '../../models/user.model';
+import { RegistrationModel } from '../../models/registration.model';
+import { Car } from '../../models/car.model';
 
 @Component({
   selector: 'driver',
@@ -10,13 +14,73 @@ import { LoginModel } from '../../models/login.model';
 })
 export class DriverComponent implements OnInit {
 
+  personalData: User;
+  carData: Car;
+  locationData: Location;
+  shouldDisplayData = false;
+  shouldDisplayCarData = false;
+
   constructor(private userService: UsersService) { }
 
   ngOnInit() {
   }
 
-  getUserByUsername(){
-    let apiMessage = new ApiMessage(localStorage.userHash, new LoginModel(null, null, localStorage.role));
-    this.userService.getUserByUsername(apiMessage).subscribe(data => {console.log(data);});
+  getMyData(){
+    if(!this.shouldDisplayData){
+      let apiMessage = new ApiMessage(localStorage.userHash, new LoginModel(null, null, localStorage.role));
+
+      this.userService.getUserByUsername(apiMessage).subscribe(
+      (data: User) =>{
+        this.personalData = data;
+        console.log(data);
+        this.shouldDisplayData = !this.shouldDisplayData;
+      },
+      error => {
+        this.shouldDisplayData = !this.shouldDisplayData;
+      });
+    }
+    else{
+      this.shouldDisplayData = !this.shouldDisplayData;
+    }
   }
+
+  changeMyData(registrationModel: RegistrationModel){
+    let updatedUser = registrationModel as User;
+    updatedUser.id = this.personalData.id;
+    updatedUser.isBanned = this.personalData.isBanned;
+    updatedUser.role = this.personalData.role;
+    updatedUser.carId = this.personalData.carId;
+    updatedUser.driverLocationId = this.personalData.driverLocationId;
+    this.userService.put(updatedUser.id, updatedUser).subscribe(
+      data => {
+        this.shouldDisplayData = !this.shouldDisplayData;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  // getMyCarData(){
+  //   if(!this.shouldDisplayCarData){
+  //     let apiMessage = new ApiMessage(localStorage.userHash, new LoginModel(null, null, localStorage.role));
+
+  //     this.userService.getUserByUsername(apiMessage).subscribe(
+  //       (data: User) =>{
+  //         this.personalData = data;
+  //         this.shouldDisplayCarData = !this.shouldDisplayCarData;
+  //       },
+  //       error => {
+  //         this.shouldDisplayCarData = !this.shouldDisplayCarData;
+  //       });
+
+  //   }
+  //   else{
+  //     this.shouldDisplayCarData = !this.shouldDisplayCarData;
+  //   }
+  // }
+
+  // changeMyCarData(){
+
+  // }
 }
