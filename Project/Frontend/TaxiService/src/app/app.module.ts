6e784c16eaf77e5f1a60f070d784d94fe05fe60f
hodoje <nikolakaraklic@gmail.com>
2018-08-06 20:37:44 +0200

    Fixing No NgModule metadata found for 'AppModule' random error. Slight code beautify.

--- Project/Frontend/TaxiService/src/app/app.module.ts
+++ Project/Frontend/TaxiService/src/app/app.module.ts
@@ -1,31 +1,32 @@
-import { LocationsService } from './services/locations.service';
-import { CarsService } from './services/cars.service';
-import { ContentGuard } from './guards/content.guard';
-import { LoginToNavbarService } from './services/login-to-navbar.service';
-import { GenericService } from './services/generic.service';
-import { RegistrationService } from './services/registration.service';
-import { UsersService } from './services/users.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { RouterModule, Router, CanActivate } from '@angular/router';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { RidesService } from './services/rides.service';
-import { LoginService } from './services/login.service';
 import { UsersComponent } from './components/users/users.component';
-import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
-import { RegistrationComponent } from './components/registration/registration.component';
 import { LoginComponent } from './components/login/login.component';
-import { FormsModule } from '@angular/forms';
 import { ContentComponent } from './components/content/content.component';
 import { CustomerComponent } from './components/customer/customer.component';
 import { DriverComponent } from './components/driver/driver.component';
 import { DispatcherComponent } from './components/dispatcher/dispatcher.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+
+import { LocationsService } from './services/locations.service';
+import { CarsService } from './services/cars.service';
+import { RidesService } from './services/rides.service';
+import { LoginService } from './services/login.service';
+import { LoginToNavbarService } from './services/login-to-navbar.service';
+import { RegistrationService } from './services/registration.service';
+import { UsersService } from './services/users.service';
+
+import { ContentGuard } from './guards/content.guard';
 import { LoginGuard } from './guards/login.guard';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterModule, Router, CanActivate } from '@angular/router';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 const Routes = [
   {
