6792b1f78b439645209add97ffe343e1e25d5546
hodoje <nikolakaraklic@gmail.com>
2018-08-21 23:50:18 +0200

    Change request message now holds only the info that is changing.

--- Project/Backend/Backend.WebApp/Models/ChangeRideRequestModel.cs
+++ Project/Backend/Backend.WebApp/Models/ChangeRideRequestModel.cs
@@ -9,22 +9,7 @@ namespace Backend.Models
 {
     public class ChangeRideRequestModel
     {
-        public int Id { get; set; }
-        public DateTime Timestamp { get; set; }
-        public int StartLocationId { get; set; }
-        public LocationDto StartLocation { get; set; }
-        public int? DestinationLocationId { get; set; }
-        public LocationDto DestinationLocation { get; set; }
-        public double Price { get; set; }
-        public string RideStatus { get; set; }
+        public LocationDto Location { get; set; }
         public string CarType { get; set; }
-        public int? CustomerId { get; set; }
-        public UserDto Customer { get; set; }
-        public int? DispatcherId { get; set; }
-        public UserDto Dispatcher { get; set; }
-        public int? DriverId { get; set; }
-        public UserDto Driver { get; set; }
-        public int? CommentId { get; set; }
-        public CommentDto Comment { get; set; }
     }
 }
