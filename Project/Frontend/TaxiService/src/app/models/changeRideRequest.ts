b39ca266fa266e90c5af84371c910443518c9479
hodoje <nikolakaraklic@gmail.com>
2018-08-22 00:05:06 +0200

    Same model change for change ride request but on FE.

--- Project/Frontend/TaxiService/src/app/models/changeRideRequest.ts
+++ Project/Frontend/TaxiService/src/app/models/changeRideRequest.ts
@@ -3,21 +3,10 @@ import { User } from './user.model';
 import { RideStatus } from "./rideStatus";
 
 export class ChangeRideRequest{
-  id: number;
-  timestamp: Date;
-  startLocationId: number;
-  startLocation: Location;
-  destinationLocationId: number;
-  destinationLocation: Location;
-  price: number;
-  rideStatus: string;
+  location: Location;
   carType: string;
-  customerId: number;
-  customer: User;
-  dispatcherId: number;
-  dispatcher: User;
-  driverId: number;
-  driver: User;
-  commentId: number;
-  comment: Comment;
+
+  constructor(){
+    this.location = new Location();
+  }
 }
